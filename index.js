467e42e541973ed69e99695c769a92f529415826
SabbirSami <smd71430@gmail.com>
2022-07-09 18:47:00 +0600

    get single product data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.up3hj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
@@ -22,6 +22,14 @@ async function run() {
 
         app.get("/products", async (req, res) => {
             const result = await productCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await productCollection
+                .find({ _id: ObjectId(id) })
+                .toArray();
             res.send(result);
         });
     } finally {
