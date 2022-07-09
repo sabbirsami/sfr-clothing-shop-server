13d0d6f75a43a6286f0b668e264b4bc36c9eca90
SabbirSami <smd71430@gmail.com>
2022-07-09 13:19:59 +0600

    connect with database

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.up3hj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("SRFstore").collection("products");
+    } finally {
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("SFR store");
 });
