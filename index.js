2777427ecb4ffa721c949a85ac456f271ca55052
SabbirSami <smd71430@gmail.com>
2022-07-14 10:11:18 +0600

    solve find order problem

--- index.js
+++ index.js
@@ -62,7 +62,9 @@ async function run() {
         // });
         app.get("/orders/:email", async (req, res) => {
             const email = req.params.email;
-            const orders = await orderCollection.findOne({ email: email });
+            const orders = await orderCollection
+                .find({ email: email })
+                .toArray();
             res.send(orders);
         });
     } finally {
