fc764df20f823987a4b0b6394cf9104ffefd1277
SabbirSami <smd71430@gmail.com>
2022-07-23 12:19:12 +0600

    create-payment-intent api ready

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +21,20 @@ async function run() {
         const productCollection = client.db("SFRstore").collection("products");
         const orderCollection = client.db("SFRstore").collection("orders");
         console.log("connected");
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const product = req.body;
+            const price = product.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            req.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 
         app.get("/products", async (req, res) => {
             const query = {};
@@ -57,9 +72,7 @@ async function run() {
 
         app.put("/orders", async (req, res) => {
             const order = req.body;
-            console.log(order);
             const newQuantity = req.body.quantity;
-
             const id = order.id;
             const query = { id: id };
             let quantity = order.quantity;
