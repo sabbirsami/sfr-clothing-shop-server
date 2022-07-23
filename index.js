9978fa69a1a543586683d2ec4cfd0f3031c64a92
SabbirSami <smd71430@gmail.com>
2022-07-23 22:53:04 +0600

    fix payment error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -24,14 +24,15 @@ async function run() {
 
         app.post("/create-payment-intent", async (req, res) => {
             const product = req.body;
-            const price = product.price;
+            console.log(product);
+            const price = product.totalCost;
             const amount = price * 100;
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: "usd",
                 payment_method_types: ["card"],
             });
-            req.send({
+            res.send({
                 clientSecret: paymentIntent.client_secret,
             });
         });
