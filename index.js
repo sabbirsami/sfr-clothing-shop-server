52ef19e1849b45a144a793c2de50125808a65dcb
SabbirSami <smd71430@gmail.com>
2022-07-27 12:09:08 +0600

    add jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
